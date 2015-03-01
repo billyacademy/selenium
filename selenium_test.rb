d1cc642be5a2bc65ff3cc0a278c194d4ded05f33
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-28 20:58:34 -0500

    Another go at selenium testing, testing to see that a page has content and doesn't have other content

--- selenium_test.rb
+++ selenium_test.rb
@@ -2,10 +2,15 @@ require "selenium-webdriver"
 
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "http://twitter.com"
+
 about_xpath = '//*[@id="front-container"]/div[4]/ul/li[1]/a'
 
 result = driver.find_element(:xpath , about_xpath)
 result.click
-puts driver.title
+
+puts "Test Passed: Page 1 Validated" if
+  /About Twitter/.match(driver.page_source)
+puts "Negative Test Passed: Page 1 Negative Test" if
+  !/About Reddit/.match(driver.page_source)
 
 driver.quit
